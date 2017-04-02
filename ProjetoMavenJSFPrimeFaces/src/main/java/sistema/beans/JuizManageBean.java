03d431eed952b1d3b995c503e2b6e30cbbf9cc68
JUNIOR-LAPTOP\junior <junior.camargo11@gmail.com>
2017-04-02 02:09:21 -0300

    inplementado o cadastro do juize começado gerenciamento de jogos

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/beans/JuizManageBean.java
@@ -0,0 +1,38 @@
+package sistema.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import sistema.modelos.Aluno;
+import sistema.modelos.Juiz;
+import sistema.service.JuizService;
+
+import java.util.List;
+
+
+@ManagedBean
+@SessionScoped
+public class JuizManageBean {
+	private Juiz juiz = new Juiz();
+	private JuizService service = new JuizService(); 
+	
+	public void salvar() {
+		
+		service.salvar(juiz);
+		juiz = new Juiz();
+	}
+
+	public Juiz getJuiz() {
+		return juiz;
+	}
+
+	public void setJuiz(Juiz juiz) {
+		this.juiz = juiz;
+	}
+	
+	public List<Juiz> getJuizes(){
+		return service.getJuizes();
+	}
+		
+
+}
