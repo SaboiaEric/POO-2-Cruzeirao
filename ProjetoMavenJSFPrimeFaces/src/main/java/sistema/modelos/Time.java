547c68460ff22a2639589bd1bfae32601cfddbae
Eric Saboia <saboia.eric@gmail.com>
2017-04-09 08:54:01 -0300

    Simplificada a tela de cadastro de time.

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Time.java
@@ -0,0 +1,27 @@
+package sistema.modelos;
+public class Time {
+
+
+	private int matricula;
+	private String nome;
+	private String diretor;
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDiretor() {
+		return diretor;
+	}
+	public void setDiretor(String diretor) {
+		this.diretor = diretor;
+	}
+
+}
