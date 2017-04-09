e661f1790ddc928598bf964d1700dc5a7999ebe9
Eric Saboia <saboia.eric@gmail.com>
2017-04-09 09:10:14 -0300

    Atualizada a tela de cadastro de elenco

--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Elenco.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Elenco.java
@@ -8,55 +8,32 @@ public class Elenco {
 	private String documento;
 	private String nome;
 	private Date dataNascimento;
-	private TipoEnum tipo;
 	private String time;
-	
-	public static enum TipoEnum{
-		Jogador,
-		Tecnico,
-		PreparadorFisico,
-		Massagista
-	}
-
 	public String getDocumento() {
 		return documento;
 	}
-
 	public void setDocumento(String documento) {
 		this.documento = documento;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
-
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-
-	public TipoEnum getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoEnum tipo) {
-		this.tipo = tipo;
-	}
-	
 	public String getTime() {
 		return time;
 	}
-
 	public void setTime(String time) {
 		this.time = time;
 	}
 
+	
 
 }
