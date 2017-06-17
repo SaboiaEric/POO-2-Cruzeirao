f3f08656d6f28d0a5b0435981653849d57eeb857
Karen <karen.torres.19@gmail.com>
2017-06-17 01:20:09 -0300

    atualizacao dos itens relacionados ao cadastro de usuario

--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Usuario.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Usuario.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 import sistema.enums.Sexo;
 
 @Entity
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"cpf"})})
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -25,6 +29,15 @@ public class Usuario implements Serializable {
 	private String email;
 	private String nome;
 	private String senha;
+	private String tipoUsuario = "NORMAL";
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
 
 	public String getSenha() {
 		return senha;
@@ -45,25 +58,26 @@ public class Usuario implements Serializable {
 	@OneToMany
 	private ArrayList<Campeonato> campeonatos = new ArrayList<Campeonato>();
 
-	// TODO private ENUM tipo
-	private String telefoneFixo;
-	private String telefoneMovel;
-	private String endereco;
-	private String rg;
+	
+	//private String telefoneFixo;
+	//private String telefoneMovel;
+	//private String endereco;
+	//private String rg;
+	@Column
 	private String cpf;
 	private String cref;
 
-	private Sexo sexo;
+	private String sexo;
 
-	public Sexo getSexo() {
+	public String getSexo() {
 		return sexo;
 	}
 
-	public void setSexo(Sexo sexo) {
+	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
 
-	private String foto;
+	
 
 	public int getCodigoUsuario() {
 		return codigoUsuario;
@@ -121,37 +135,6 @@ public class Usuario implements Serializable {
 		this.campeonatos = campeonatos;
 	}
 
-	public String getTelefoneFixo() {
-		return telefoneFixo;
-	}
-
-	public void setTelefoneFixo(String telefoneFixo) {
-		this.telefoneFixo = telefoneFixo;
-	}
-
-	public String getTelefoneMovel() {
-		return telefoneMovel;
-	}
-
-	public void setTelefoneMovel(String telefoneMovel) {
-		this.telefoneMovel = telefoneMovel;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getRg() {
-		return rg;
-	}
-
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
 
 	public String getCpf() {
 		return cpf;
@@ -169,12 +152,35 @@ public class Usuario implements Serializable {
 		this.cref = cref;
 	}
 
-	public String getFoto() {
-		return foto;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		return result;
 	}
 
-	public void setFoto(String foto) {
-		this.foto = foto;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		return true;
 	}
+
+	
+	
+	
+	
+	
 
 }
